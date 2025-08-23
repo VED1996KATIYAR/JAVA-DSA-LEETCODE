7349c526d9994844eb76837dc7395e9c94c499cf
VED1996KATIYAR <ved750katiyar@gmail.com>
2025-08-24 00:39:23 +0530

    Time: 0 ms (100%), Space: 40.9 MB (61.57%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n!=1){
+            if(n%4==0){
+                n/=4;
+            }else{
+                break;
+            }
+        }
+        return n==1;
+    }
+}
