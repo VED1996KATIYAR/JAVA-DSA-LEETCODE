f4a10ad22aa5113e9061d174aa34c889ccbe89d9
VED1996KATIYAR <ved750katiyar@gmail.com>
2025-08-26 00:57:06 +0530

    Time: 1 ms (84.2%), Space: 41.6 MB (6.46%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,23 +1,17 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==2147483647){
-            return 46340;
-        }
-        int a=0;
-        int mid=x/2 + 1;
-        int num=0;
-        while(a<=mid){
-            int square=a*a;
-            if(square==x){
-                num=a;
-                break;
-            }else if(square > x){
-                num=a-1;
-                break;
+        long start=0;
+        long end=x;
+        while(start<=end){
+            long mid=(start+end)/2;
+            if(mid*mid==x){
+                return (int) mid;
+            }else if(mid*mid>x){
+                end=mid-1;
             }else{
-                a+=1;
+                start=mid+1;
             }
-        }     
-        return num;  
+        }
+        return (int)end;
     }
 }
