97b5c9b6c952a48062bb33c9de7447f5cd8cf175
VED1996KATIYAR <ved750katiyar@gmail.com>
2025-08-26 11:14:18 +0530

    Time: 4 ms (52.56%), Space: 40.4 MB (63.34%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int start=0;
+        long end=(long)Math.sqrt(c);
+        while(start<=end){
+            if((start*start+end*end)==c){
+                return true;
+            }else if((start*start+end*end)>c){
+                end--;
+            }else{
+                start++;
+            }
+        }
+        return false;
+    }
+}
